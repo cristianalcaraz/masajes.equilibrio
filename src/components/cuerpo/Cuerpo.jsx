e20e3195ac573ffa6004cc99bc7cc0f514238ac3
Your Name <your.email@example.com>
2025-04-20 15:30:02 -0300

    Actualizo App.jsx y configuro routing

--- src/components/cuerpo/Cuerpo.jsx
+++ src/components/cuerpo/Cuerpo.jsx
@@ -5,7 +5,7 @@ const Cuerpo = () => {
     <div>
       <div className="cards-container">
         <div className="card">
-          <img src="/img/close.jpg" className="card-img-top" alt="masaje de espalda" />
+          <img src="/masajes.equilibrio/img/close.jpg" className="card-img-top" alt="masaje de espalda" />
           <div className="card-body">
             <h2>Masaje Relajante</h2>
             <p className="relajante">
@@ -14,7 +14,7 @@ const Cuerpo = () => {
           </div>
         </div>
         <div className="card">
-          <img src="/img/efusor.jpeg" className="card-img-top" alt="masaje de espalda" />
+          <img src="/masajes.equilibrio/img/efusor.jpeg" className="card-img-top" alt="efusor" />
           <div className="card-body">
             <h2>Descontracturante</h2>
             <p className="Masaje-descontracturante">
@@ -23,14 +23,14 @@ const Cuerpo = () => {
           </div>
         </div>
         <div className="card">
-          <img src="/img/masaje-espalda.jpg" className="card-img-top" alt="masaje de espalda" />
+          <img src="/masajes.equilibrio/img/masaje-espalda.jpg" className="card-img-top" alt="masaje de espalda" />
           <div className="card-body">
             <h2>Masaje de Espalda</h2>
             <p>Alivio del dolor y tensión muscular, reducción de las contracturas musculares y mayor flexibilidad.</p>
           </div>
         </div>
         <div className="card">
-          <img src="/img/you.jpg" className="card-img-top" alt="efusor" />
+          <img src="/masajes.equilibrio/img/you.jpg" className="card-img-top" alt="reflexología" />
           <div className="card-body">
             <h2>Reflexología</h2>
             <p className="espalda">
@@ -39,70 +39,63 @@ const Cuerpo = () => {
           </div>
         </div>
         <div className="card">
-          <img src="/img/masaje-relajante.jpg" className="card-img-top" alt="efusor" />
+          <img src="/masajes.equilibrio/img/masaje-relajante.jpg" className="card-img-top" alt="masaje relajante" />
           <div className="card-body">
             <h2>Masaje Muscular</h2>
             <p className="Masaje Muscular">
-              Libera contracturas,mejora la movilidad ,relaja la musculatura en profundidad y libera la tensiones acumuladas
+              Libera contracturas, mejora la movilidad, relaja la musculatura en profundidad y libera las tensiones acumuladas.
             </p>
           </div>
         </div>
-
       </div>
-
-
 
       <div>
+        <img src="/masajes.equilibrio/img/masaje-descontracturante.jpg" alt="imagen-fija" />
+        <h2>¿Qué son los Masajes Relajantes?</h2>
+        <p>
+          El masaje relajante es un tipo de masaje suave que está basado en los principios de la fisioterapia, la movilización de los tejidos musculares, fasciales y la regulación del sistema nervioso.
+        </p>
+        <p>
+          En el masaje relajante se emplean maniobras superficiales en las que la intensidad de la presión es suave, con ritmo lento y reiterativo, de manera que, al recibir un contacto repetido, se pierde la sensación de dolor y los músculos se relajan.
+        </p>
 
-        <img src="/img/masaje-descontracturante.jpg" alt="imagen-fija" />
-        <h2>¿Que son los Masajes Relajantes ?</h2>
-        <p>El masaje relajante es un tipo de masaje suave que está basado en los principios de la fisioterapia, la movilización de los tejidos musculares, fasciales y la regulación del sistema nervioso.
-
-          En el masaje relajante se emplean maniobras superficiales en las que la intensidad de la presión es suave, con ritmo lento y reiterativo, de manera que, al recibir un contacto repetido, se pierde la sensación de dolor y los músculos se relajan.</p>
-        <h2>Beneficios del Masaje Relajante </h2>
-        <p>Reduce el estrés y la ansiedad
-          ✔️Alivia dolores y tensiones musculares
-          ✔️ Mejora la circulación y el sistema inmune
-          ✔️Elimina toxinas y mejora tu energía
-          ✔️ Promueve relajación, equilibrio y bienestar emocional</p>
+        <h2>Beneficios del Masaje Relajante</h2>
+        <p>
+          ✔️ Reduce el estrés y la ansiedad <br />
+          ✔️ Alivia dolores y tensiones musculares <br />
+          ✔️ Mejora la circulación y el sistema inmune <br />
+          ✔️ Elimina toxinas y mejora tu energía <br />
+          ✔️ Promueve relajación, equilibrio y bienestar emocional
+        </p>
 
         <h2>Masaje Descontracturante</h2>
-        <p>Ideal para liberar tensiones acumuladas por el estrés, malas posturas o sobrecarga muscular.
+        <p>
+          ✔️ Alivia contracturas y rigidez <br />
+          ✔️ Reduce el dolor muscular <br />
+          ✔️ Mejora la circulación y movilidad <br />
+          ✔️ Relaja cuerpo y mente <br />
+          Tu cuerpo te lo va a agradecer!
+        </p>
 
-          ✔️Alivia contracturas y rigidez
-          ✔️ Reduce el dolor muscular
-          ✔️ Mejora la circulación y movilidad
-          ✔️Relaja cuerpo y mente
-
-          #Sentí el alivio desde la primera sesión.
-          Tu cuerpo te lo va a agradecer!</p>
         <h2>Masaje de Espalda</h2>
-        <p>💆‍♀️✨ Masaje de Espalda: alivio inmediato ✨💆‍♂️
-
-          La espalda acumula el estrés del día a día.
-          Este masaje es perfecto para:
-
-          ✔️ Aliviar tensiones y contracturas
-          ✔️ Reducir el dolor muscular
-          ✔️ Mejorar la postura y movilidad
+        <p>
+          💆‍♀️✨ Masaje de Espalda: alivio inmediato ✨💆‍♂️ <br />
+          ✔️ Aliviar tensiones y contracturas <br />
+          ✔️ Reducir el dolor muscular <br />
+          ✔️ Mejorar la postura y movilidad <br />
           ✔️ Relajar cuerpo y mente
+        </p>
 
-          🌿 Regalale a tu espalda el cuidado que se merece</p>
-        <h2>Binestar desde tus pies Reflexologia </h2>
-        <p>La reflexología estimula puntos en los pies que conectan con todo tu cuerpo.
-          Un masaje que alivia, equilibra y revitaliza.
-
-          ✔️Reduce el estrés
-          ✔️Mejora la circulación
-          ✔️Alivia dolores
-          ✔️Armoniza cuerpo y mente</p>
-
-
+        <h2>Bienestar desde tus pies - Reflexología</h2>
+        <p>
+          ✔️ Reduce el estrés <br />
+          ✔️ Mejora la circulación <br />
+          ✔️ Alivia dolores <br />
+          ✔️ Armoniza cuerpo y mente
+        </p>
       </div>
-
     </div>
-
-  );
+  )
 }
 
-export default Cuerpo;
+export default Cuerpo
