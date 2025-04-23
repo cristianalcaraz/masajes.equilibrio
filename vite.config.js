c94fe1f1e11512bad744da9552794d1d09a02b12
Emiliano Alcaraz <emilianoalcaraz@hotmail.com>
2025-04-23 20:44:22 -0300

    updating

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: 'https://cristianalcaraz.github.io/masajes.equilibrio',
+  base: '/masajes.equilibrio/',
   plugins: [react()],
 })
